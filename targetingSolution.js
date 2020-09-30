e3c58744685552fb433bbbf97bb08cb8934bf66c
Tommy Ryan <tommy.ryan@servicenow.com>
2020-09-30 13:44:01 -0700

    feat: laser 3

--- targetingSolution.js
+++ targetingSolution.js
@@ -1,3 +1,19 @@
 class TargetingSolution {
-  constructor
+  constructor(coords) {
+    this.x = coords.x;
+    this.y = coords.y;
+    this.z = coords.z;
+  }
+
+  target() {
+    return `(${this.x}, ${this.y}, ${this.z})`;
+  }
 }
+
+const sln = new TargetingSolution({
+  x: 45,
+  y: 12,
+  z: -1
+});
+
+console.log(sln.target());
